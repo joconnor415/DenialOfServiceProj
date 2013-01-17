b677acff43c11602c176b2d272f5254f62d34c66
Jeremiah O'Connor <j.oconnor415@gmail.com>
2013-01-17 12:10:05 -0800

    Initial commit

---
+++ src/ThreadEx.java
@@ -0,0 +1,18 @@
+
+//public class ThreadEx {
+//
+//	public static void main (String args[])
+//	{
+//		//for loop creating 100 new thread objects which send request to server
+//		Thread t1= new Thread(new Client("one"));
+//		Thread t2= new Thread(new Client("two"));
+//		Thread t3= new Thread(new Client("three"));
+//		Thread t4= new Thread(new Client("four"));
+//		
+//		t1.start();
+//		t2.start();
+//		t3.start();
+//		t4.start();
+//		
+//}
+//}
